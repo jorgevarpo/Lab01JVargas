e1470a91442af63b9e9bc6ccedaf8bd389dfa50d
Jorgevarpo <jorgevarpo2@gmail.com>
2018-09-18 00:28:26 -0600

    Initial commit

---
+++ Lab01.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab01JorgeVargas;
+
+namespace Lab01.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Person");
+            // Person person = new Person();
+
+            //person.Id = 1;
+            //person.Name = "Juan";
+            //person.LastName = "Perez";
+
+            //Segunda Forma
+
+            Person person = new Person(1, "Juan", "Perez");
+
+            //ILogger logger = person;
+            //Tercera Forma
+
+            //Person person = new Person
+            //{
+             //   Id = 1,
+             //   Name = "Juan",
+             //   LastName = "Perez"
+            //};
+
+
+            string personString = string.Format("Id:{0}, Name: {1}, LastName:{2}", person.Id, person.Name, person.LastName);
+            System.Console.WriteLine(personString);
+            person.log("Hola");
+            System.Console.ReadKey();
+
+        }
+    }
+
+    internal interface ILogger
+    {
+    }
+}
---
+++ Lab01JorgeVargas/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01JorgeVargas
+{
+    public class Person: iLogger
+    {
+        public Person()
+        {
+        }
+        public Person(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public void Log(string valor)
+        {
+            System.Console.WriteLine(valor);
+        }
+
+        public void log(string valor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
